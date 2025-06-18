1289684341e2ff2330e0246e60d9a7adb17ae7f2
surbhisaini123 <shurbhisaini206@gmail.com>
2025-06-18 22:03:46 +0530

    java

--- patternno.java
+++ patternno.java
@@ -1,5 +1,21 @@
 public class patternno {
     public static void main(String[] args) {
-        int [][] arr={1,}
+        int row=3;
+        int col=3;
+        int num=1;
+        int [][] arr= new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                arr[i][j]=num;
+                num++;
+            }
+        }
+        System.out.println("the 2d array is:");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.println(arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
